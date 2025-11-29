2d05bebd48597452afa8981b7a119cdad744f4ed
vhanok70 <vhanok70@gmail.com>
2025-11-29 21:28:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// IMPORTANT: use your repo name here
 export default defineConfig({
+  base: "/Hanok-global-trader/",
   plugins: [react()],
-  base: '/Hanok-global-trader/',   // <— IMPORTANT for GitHub Pages
-})
+});
